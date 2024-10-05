6e9bd46c1d956a7b527dcd1297b0bf7d28a129cb
jtsekhansky <jtsekhansky@gmail.com>
2024-10-04 21:17:12 -0400

    typo fix and test

--- setup.sql
+++ setup.sql
@@ -1,12 +1,12 @@
 DROP DATABASE IF EXISTS passwords;
 
-CREATE DATABASE `passwords` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE DATABASE passwords DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
-CREATE TABLE 'passwords'.'websites' (
+CREATE TABLE passwords.websites (
     website_name    VARCHAR(100)    NOT NULL,
     website_url     VARCHAR(128)    NOT NULL,
     PRIMARY KEY (website_name)
-)
+);
 
 Insert into passwords.websites
  (website_name, website_url)
@@ -20,4 +20,4 @@ Insert into passwords.websites
  ("BJs wholesale club", "https://www.bjs.com"),
  ("Stop and Shop", "https://stopandshop.com"),
  ("Costco", "https://www.costco.com"),
- ("Amazon", "https://www.amazon.com")
+ ("Amazon", "https://www.amazon.com");
