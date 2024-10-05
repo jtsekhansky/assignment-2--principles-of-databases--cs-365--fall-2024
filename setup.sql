1665425d1d78c1639c7eadc2c2ac7f9f4d97447f
jtsekhansky <jtsekhansky@gmail.com>
2024-10-04 21:50:47 -0400

    created and populated users table

--- setup.sql
+++ setup.sql
@@ -3,9 +3,17 @@ DROP DATABASE IF EXISTS passwords;
 CREATE DATABASE passwords DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 CREATE TABLE passwords.websites (
-    website_name    VARCHAR(100)    NOT NULL,
+    website_name    VARCHAR(100)    ,
     website_url     VARCHAR(128)    NOT NULL,
     PRIMARY KEY (website_name)
+);
+
+CREATE TABLE passwords.users (
+    user_name        VARCHAR(32)    ,
+    first_name      VARCHAR(32)     NOT NULL,
+    last_name       VARCHAR(32)     NOT NULL,
+    email           VARCHAR(320)    NOT NULL,
+    PRIMARY KEY (user_name)
 );
 
 Insert into passwords.websites
@@ -21,3 +29,17 @@ Insert into passwords.websites
  ("Stop and Shop", "https://stopandshop.com"),
  ("Costco", "https://www.costco.com"),
  ("Amazon", "https://www.amazon.com");
+
+ Insert into passwords.users
+ (user_name, first_name, Last_name, email)
+ values
+("jtisawesome", "jacob", "tsekansky", "j@bing.com"),
+("collegegood", "jacob", "tsekansky", "j@h0rtfurd.com"),
+("schoolgreat", "david", "tsekansky", "d@cheshire.com"),
+("blackbelt", "joe", "schmoe", "j@schmoe.com"),
+("bookwarrior", "rahn", "kazowski", "rkazowski@books.org"),
+("bestdriver", "lexi", "brimmings", "lbrims@toyota.com"),
+("gurl479", "leah", "stephens", "stephens@gmail.com"),
+("saleshopstop", "brenna", "mullings", "mulls@stopshop.com"),
+("savingbrands", "rahn", "kazowski", "kaz@brand.com"),
+("warehouselifter", "lexi", "brimmings", "bestlift@atoz.com");
